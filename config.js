e0af739f22fa14f71e3cbcb4190b8edd63450ecf
trenoty <112545828+trenoty@users.noreply.github.com>
2024-05-28 02:13:47 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    titleWeb: "For you, My Love💗💖",
+    introTitle: 'Hi love HAHAHA',
+    introDesc: 'I just wanted you to know that i love you very very much more than I can ever express in my whole life.,
+    btnIntro: 'mwuahhh😙😙',
+    title: 'This is quite sudden but would you want to be my girlfriend?🥰',
+    desc: 'If your answer is no, I respect your decision🥰 ',
+    btnYes: 'YES, I LOVE YOU <33',
+    btnNo: 'NO :3',
+    question: 'There are more than 8 billion people in the world, and you chose me. I just really really love you my honey bunch😘😘',
+    btnReply: 'Send',
+    reply: 'Loving you is as natural as breathing, it's something I do with out thinking, a beautiful part of who I am🥰',
+    mess: 'I love you more than these words could ever express. You're my everything my love',
+    messDesc: 'I am so happy tonight so I will spam with you with kisses😙😙😙',
+    btnAccept: 'mwuahhhh😘😘',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
